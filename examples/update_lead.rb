4d78653a1bf9917835cbb3e5cfc7a31f5382c1ce
jalemieux@gmail.com <jalemieux@gmail.com>
2014-03-18 18:16:42 -0700

    added doc

--- examples/update_lead.rb
+++ examples/update_lead.rb
@@ -1,17 +1,28 @@
 require 'mkto_rest'
 require 'yaml'
 
+=begin
+This script looks for the hostname, client id and key in .mktorest. 
+Create that file with the following (yaml) format:
+  
+    ---
+    :hostname: ''
+    :client_id: ''
+    :client_secret: ''
+
+set your hostname, client id and key to the right values.
+=end
+
 config_path = File.expand_path(File.join(File.dirname(__FILE__),'..', '.mktorest'))
 
-if File.exists? config_path
+if File.exists? config_path 
   config = YAML::load_file(config_path) 
 else
-  File.open(config_path, 'w') {|f| f.write({ hostname: '', client_id: '', client_secret: '' }.to_yaml) }
-  p "Set your hostname, client id and key in #{config_path}"
+  print "Set your hostname, client id and key in #{config_path}\n\n in this format\n #{{ hostname: '', client_id: '', client_secret: '' }.to_yaml}\n\n"
   exit 1
 end
 if ARGV.size < 2 or ARGV[0].include?('=')
-  print "#{__FILE__} <lead_email> <key1:value2> <key2:value2> ...\n    e.g.: #{__FILE__} CS-Login=john@acme.com CS-EmailNames=Production\n\n"
+  print "#{__FILE__} <lead_email> <key1:value2> <key2:value2> ...\n    e.g.: #{__FILE__} john@acme.com CS-Login=john@acme.com CS-EmailNames=Production\n\n"
   exit 1
 end
 
