4ffe6e167d0108e86b5cb0a299b3ebd40e2eb037
jalemieux@gmail.com <jalemieux@gmail.com>
2014-05-08 16:41:47 -0700

    removed test spec

--- spec/test_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-
-describe MktoRest do
-  it "brouillon" do
-    stub_request(:any, "www.example.com")
-    Net::HTTP.get("www.example.com", "/")    # ===> Success
-  end
-end
