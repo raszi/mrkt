23f83eb32c0c1d3fde9a2f2dad670646af904736
Krisztian Szabo <kszabo@whitepages.com>
2015-03-16 17:41:37 +0100

    More mkto_rest_helper style fixes

--- spec/mkto_rest_helper.rb
+++ spec/mkto_rest_helper.rb
@@ -1,13 +1,13 @@
-def set_authentication_stub_request(hostname, client_id, client_key, token)
+def set_authentication_stub_request(hostname, client_id, client_key)
   body = {
-    access_token: "e58af350-6c40-453f-8883-35f12d2e8742:qe",
-    token_type: "bearer",
+    access_token: 'e58af350-6c40-453f-8883-35f12d2e8742:qe',
+    token_type: 'bearer',
     expires_in: 86183,
-    scope: "ser@user.com"
+    scope: 'ser@user.com'
   }
 
   stub_request(:get, "https://#{hostname}/identity/oauth/token?client_id=#{client_id}&client_secret=#{client_key}&grant_type=client_credentials")
-    .with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'})
+    .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
     .to_return(
       status: 200,
       body: body.to_json,
@@ -18,35 +18,34 @@ end
 def set_create_leads_stub_request(leads, hostname, token, options = nil)
   # Endpoint expects these headers
   headers = {
-    "Authorization" => "Bearer #{token}",
-    'Content-Type'=>'application/json',
-    'Accept'=>'*/*',
-    'User-Agent'=>'Ruby'
+    'Authorization' => "Bearer #{token}",
+    'Content-Type' => 'application/json',
+    'Accept' => '*/*',
+    'User-Agent' => 'Ruby'
   }
   url = "https://#{hostname}/rest/v1/leads.json"
   # taken from dev.marekto.com
   req_body = {
-    "action" => "createOnly",
-    "input" => []
+    'action' => 'createOnly',
+    'input' => []
   }
   if options
     req_body['partitionName'] = options[:partition] if options[:partition]
     req_body['lookupField'] = options[:lookupField] if options[:lookupField]
   end
   leads.each do |l|
-    req_body["input"] << l
+    req_body['input'] << l
   end
 
   # talen from dev.marketo.com
   resp_body = {
-    "requestId" => "e42b#14272d07d78",
-    "success" => true,
-    "result" => []
+    'requestId' => 'e42b#14272d07d78',
+    'success' => true,
+    'result' => []
   }
-  stub_request(:post, url).with(:headers => headers, :body => req_body.to_json)
-    .to_return(:status => 200, :body => resp_body.to_json, :headers => {})
+  stub_request(:post, url).with(headers: headers, body: req_body.to_json)
+    .to_return(status: 200, body: resp_body.to_json, headers: {})
 end
-
 
 def set_get_leads_stub_request(type, email, hostname, token)
   url = "https://#{hostname}/rest/v1/leads.json?access_token=#{token}&filterType=#{type}&filterValues=#{email}"
@@ -63,25 +62,24 @@ def set_get_leads_stub_request(type, email, hostname, token)
     ]
   }
 
-  stub_request(:get, url).with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'})
-    .to_return(:status => 200, :body => body.to_json )
+  stub_request(:get, url).with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
+    .to_return(status: 200, body: body.to_json)
 end
 
 def set_update_lead_stub_request(type, value, fields, hostname, token)
-
   # Endpoint expect this body
   req_body = {
-    action: "updateOnly",
+    action: 'updateOnly',
     lookupField: type.to_s,
-    input: [ { type => value  }.merge(fields) ]
+    input: [{ type => value }.merge(fields)]
   }.to_json
 
   # Endpoint expects these headers
   headers = {
-    "Authorization" => "Bearer #{token}",
-    'Content-Type'=>'application/json',
-    'Accept'=>'*/*',
-    'User-Agent'=>'Ruby'
+    'Authorization' => "Bearer #{token}",
+    'Content-Type' => 'application/json',
+    'Accept' => '*/*',
+    'User-Agent' => 'Ruby'
   }
 
   # response expected body
@@ -96,6 +94,6 @@ def set_update_lead_stub_request(type, value, fields, hostname, token)
   }.to_json
 
   stub_request(:post, "https://#{hostname}/rest/v1/leads.json")
-    .with(:headers => headers,:body => req_body)
-    .to_return(:status => 200, :body => resp_body )
+    .with(headers: headers, body: req_body)
+    .to_return(status: 200, body: resp_body)
 end
