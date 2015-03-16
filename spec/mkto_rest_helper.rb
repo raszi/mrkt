569544585d554055a882727a5070aeb0113ae787
Krisztian Szabo <kszabo@whitepages.com>
2015-03-16 17:25:39 +0100

    Indent and whitespace mkto_rest_helper properly

--- spec/mkto_rest_helper.rb
+++ spec/mkto_rest_helper.rb
@@ -1,6 +1,3 @@
-
-
-
 def set_authentication_stub_request(hostname, client_id, client_key, token)
   body = {
     access_token: "e58af350-6c40-453f-8883-35f12d2e8742:qe",
@@ -10,10 +7,12 @@ def set_authentication_stub_request(hostname, client_id, client_key, token)
   }
 
   stub_request(:get, "https://#{hostname}/identity/oauth/token?client_id=#{client_id}&client_secret=#{client_key}&grant_type=client_credentials")
-   .with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'})
-   .to_return(:status => 200,
-              :body => body.to_json,
-              :headers => {})
+    .with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'})
+    .to_return(
+      status: 200,
+      body: body.to_json,
+      headers: {}
+    )
 end
 
 def set_create_leads_stub_request(leads, hostname, token, options = nil)
@@ -27,8 +26,8 @@ def set_create_leads_stub_request(leads, hostname, token, options = nil)
   url = "https://#{hostname}/rest/v1/leads.json"
   # taken from dev.marekto.com
   req_body = {
-   "action" => "createOnly",
-   "input" => []
+    "action" => "createOnly",
+    "input" => []
   }
   if options
     req_body['partitionName'] = options[:partition] if options[:partition]
@@ -40,9 +39,9 @@ def set_create_leads_stub_request(leads, hostname, token, options = nil)
 
   # talen from dev.marketo.com
   resp_body = {
-   "requestId" => "e42b#14272d07d78",
-   "success" => true,
-   "result" => []
+    "requestId" => "e42b#14272d07d78",
+    "success" => true,
+    "result" => []
   }
   stub_request(:post, url).with(:headers => headers, :body => req_body.to_json)
     .to_return(:status => 200, :body => resp_body.to_json, :headers => {})
@@ -51,7 +50,6 @@ end
 
 def set_get_leads_stub_request(type, email, hostname, token)
   url = "https://#{hostname}/rest/v1/leads.json?access_token=#{token}&filterType=#{type}&filterValues=#{email}"
-
 
   # expected body
   body = {
@@ -66,7 +64,7 @@ def set_get_leads_stub_request(type, email, hostname, token)
   }
 
   stub_request(:get, url).with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'})
-   .to_return(:status => 200, :body => body.to_json )
+    .to_return(:status => 200, :body => body.to_json )
 end
 
 def set_update_lead_stub_request(type, value, fields, hostname, token)
@@ -75,7 +73,7 @@ def set_update_lead_stub_request(type, value, fields, hostname, token)
   req_body = {
     action: "updateOnly",
     lookupField: type.to_s,
-      input: [ { type => value  }.merge(fields) ]
+    input: [ { type => value  }.merge(fields) ]
   }.to_json
 
   # Endpoint expects these headers
@@ -91,13 +89,13 @@ def set_update_lead_stub_request(type, value, fields, hostname, token)
     requestId: 1,
     success: true,
     result: [
-     {
-      type.to_s => value
-     }
-    ]}.to_json
+      {
+        type.to_s => value
+      }
+    ]
+  }.to_json
 
   stub_request(:post, "https://#{hostname}/rest/v1/leads.json")
-   .with(:headers => headers,:body => req_body)
-   .to_return(:status => 200, :body => resp_body )
+    .with(:headers => headers,:body => req_body)
+    .to_return(:status => 200, :body => resp_body )
 end
-
