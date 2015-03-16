05e17b6fddee30f412f2a2a5d8721c75d566e531
Krisztian Szabo <kszabo@whitepages.com>
2015-03-16 17:34:41 +0100

    Clean up lead code

--- lib/mkto_rest/lead.rb
+++ lib/mkto_rest/lead.rb
@@ -4,29 +4,29 @@ module MktoRest
     def initialize(client, args)
       @vars = []
       @client = client
-      args.each do |k,v|
+      args.each do |k, v|
         @vars << k
-        self.instance_variable_set("@#{k}", v)
+        instance_variable_set("@#{k}", v)
       end
     end
 
     def method_missing(mthsym, *args)
-      if @vars.include? mthsym
-        return self.instance_variable_get("@#{mthsym}") if args.empty?
-        self.instance_variable_set("@#{mthsym}", args[0])
-      end
+      return unless @vars.include?(mthsym)
+      return instance_variable_get("@#{mthsym}") if args.empty?
+      instance_variable_set("@#{mthsym}", args[0])
     end
 
     def update(args, attr = :id)
-      if attr == :id
-        @client.update_lead_by_id self.id, args
-      elsif attr == :email
-        @client.update_lead_by_email self.email, args
+      case attr
+      when :id
+        @client.update_lead_by_id(id, args)
+      when :email
+        @client.update_lead_by_email(email, args)
       end
     end
 
     def to_s
-      @vars.map { |k| "#{k} => #{self.send(k)}" }.join(", ")
+      @vars.map { |k| "#{k} => #{send(k)}" }.join(', ')
     end
   end
 end
