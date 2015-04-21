b2435674147dba1ecc986dadaaf3a7577ed1e46d
KARASZI István <github@spam.raszi.hu>
2015-04-21 13:12:09 +0200

    add codeclimate

--- mrkt.gemspec
+++ mrkt.gemspec
@@ -28,4 +28,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'webmock', '~> 1.21.0'
   spec.add_development_dependency 'simplecov', '~> 0.10.0'
   spec.add_development_dependency 'pry-byebug', '~> 3.1.0'
+  spec.add_development_dependency 'codeclimate-test-reporter', '~> 0.4.7'
+
+
+gem "codeclimate-test-reporter", require: false
 end
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,10 +1,16 @@
 require 'webmock/rspec'
 
-RSpec.configure do
+RSpec.configure do |config|
   def json_stub(content_stub)
     {
       headers: { content_type: 'application/json' },
       body: JSON.generate(content_stub)
     }
+  end
+
+  if ENV['CODECLIMATE_REPO_TOKEN']
+    config.after(:suite) do
+      WebMock.disable_net_connect!(allow: 'codeclimate.com') 
+    end
   end
 end
