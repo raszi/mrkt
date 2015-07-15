f60a847f2df952d145febf5f15e8e0b509cc6cf6
alex-wood <alex.wood.86@gmail.com>
2015-07-15 14:12:22 -0700

    Use store instead of file path for flexibility

--- lib/mrkt/concerns/connection.rb
+++ lib/mrkt/concerns/connection.rb
@@ -7,8 +7,8 @@ module Mrkt
     end
 
     def init_connection
-      # Provide CA certificates for use with HTTPS (more info here https://github.com/lostisland/faraday/wiki/Setting-up-SSL-certificates):
-      Faraday.new(:url => "https://#{@host}", :ssl => { :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}) do |conn|
+      # Provide CA certificates for use with HTTPS:
+      Faraday.new(:url => "https://#{@host}", :ssl => { :cert_store => OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE }) do |conn|
         conn.request :multipart
         conn.request :url_encoded
 
