4ee584ee17ba69c1160ca80c556cf92bcd3feed3
KARASZI István <github@spam.raszi.hu>
2016-11-01 23:20:53 +0100

    Use raise only instead of raise + fail

--- lib/mrkt/faraday_middleware/response.rb
+++ lib/mrkt/faraday_middleware/response.rb
@@ -12,8 +12,8 @@ module Mrkt
 
         data = env[:body]
 
-        fail Mrkt::Errors::EmptyResponse if data.nil?
-        fail Mrkt::Errors::Error, data[:error_description] if data.key?(:error)
+        raise Mrkt::Errors::EmptyResponse if data.nil?
+        raise Mrkt::Errors::Error, data[:error_description] if data.key?(:error)
 
         handle_errors!(data[:errors]) unless data.fetch(:success, true)
       end
@@ -21,8 +21,8 @@ module Mrkt
       def handle_errors!(errors)
         error = errors.first
 
-        fail Mrkt::Errors::Unknown if error.nil?
-        fail Mrkt::Errors.find_by_response_code(error[:code].to_i), error[:message]
+        raise Mrkt::Errors::Unknown if error.nil?
+        raise Mrkt::Errors.find_by_response_code(error[:code].to_i), error[:message]
       end
     end
   end
