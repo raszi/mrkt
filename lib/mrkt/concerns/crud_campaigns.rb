f4de0588e77886548f0ac8e325f8e42067d92bda
Emir Aydin <emir.aydin@hootsuite.com>
2017-07-14 20:56:46 -0700

    added method to get campaign by id

--- lib/mrkt/concerns/crud_campaigns.rb
+++ lib/mrkt/concerns/crud_campaigns.rb
@@ -1,5 +1,10 @@
 module Mrkt
   module CrudCampaigns
+
+    def get_campaign(id)
+      get("/rest/v1/campaigns/#{id}.json")
+    end
+
     def request_campaign(id, lead_ids, tokens = {})
       post("/rest/v1/campaigns/#{id}/trigger.json") do |req|
         params = {
