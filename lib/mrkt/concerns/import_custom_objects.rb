071e2850ad84b49325b30c2f5a6bb11b29daec7b
ccelestial <celestialchristopher@gmail.com>
2017-10-26 14:28:32 +0800

    Change name of methods

--- lib/mrkt/concerns/import_custom_objects.rb
+++ lib/mrkt/concerns/import_custom_objects.rb
@@ -1,23 +1,23 @@
 module Mrkt
   module ImportCustomObjects
-    def import_custom_objects(file, api_name, format = 'csv')
+    def import_custom_object(file, api_name, format = 'csv')
       params = {
         format: format,
         file: Faraday::UploadIO.new(file, 'text/csv')
       }
 
-      post('/bulk/v1/customobjects/#{api_name}/import.json', params)
+      post("/bulk/v1/customobjects/#{api_name}/import.json", params)
     end
 
-    def import_custom_objects_status(id, api_name)
+    def import_custom_object_status(id, api_name)
       get("/bulk/v1/customobjects/#{api_name}/import/#{id}/status.json")
     end
 
-    def import_custom_objects_failures(id, api_name)
+    def import_custom_object_failures(id, api_name)
       get("/bulk/v1/customobjects/#{api_name}/import/#{id}/failures.json")
     end
 
-    def import_custom_objects_warnings(id, api_name)
+    def import_custom_object_warnings(id, api_name)
       get("/bulk/v1/customobjects/#{api_name}/import/#{id}/warnings.json")
     end
   end
