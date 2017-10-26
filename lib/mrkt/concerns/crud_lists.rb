07a88f6b5ee94454d269f72fcc213b6bc9b567cb
ccelestial <celestialchristopher@gmail.com>
2017-10-26 09:00:29 +0800

    Added get for all the static list collection

--- lib/mrkt/concerns/crud_lists.rb
+++ lib/mrkt/concerns/crud_lists.rb
@@ -9,6 +9,22 @@ module Mrkt
       get("/rest/v1/list/#{list_id}/leads.json", params)
     end
 
+    def get_list(list_id)
+      get("/rest/v1/lists/#{list_id}.json")
+    end
+
+    def get_lists(id: nil, name: nil, program_name: nil, workspace_name: nil, batch_size: nil, next_page_token: nil)
+      params = {}
+      params[:id] if id
+      params[:name] = name if name
+      params[:programName] = program_name if program_name
+      params[:workspaceName] = workspace_name if workspace_name
+      params[:batchSize] = batch_size if batch_size
+      params[:nextPageToken] = next_page_token if next_page_token
+
+      get("/rest/v1/lists.json", params)
+    end
+
     def add_leads_to_list(list_id, lead_ids)
       post("/rest/v1/lists/#{list_id}/leads.json") do |req|
         params = {
