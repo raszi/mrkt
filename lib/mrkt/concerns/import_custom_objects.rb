b89ca191a602bc898cb169ad43e93c61599605d4
ccelestial <celestialchristopher@gmail.com>
2017-10-26 09:00:56 +0800

    Bulk import for custom objects feature

---
+++ lib/mrkt/concerns/import_custom_objects.rb
@@ -0,0 +1,24 @@
+module Mrkt
+  module ImportCustomObjects
+    def import_custom_objects(file, format = 'csv', api_name:)
+      params = {
+        format: format,
+        file: Faraday::UploadIO.new(file, 'text/csv')
+      }
+
+      post('/bulk/v1/customobjects/#{api_name}/import.json')
+    end
+
+    def import_custom_objects_status(id, api_name)
+      get("/bulk/v1/customobjects/#{api_name}/import/#{id}/status.json")
+    end
+
+    def import_custom_objects_failures(id, api_name)
+      get("/bulk/v1/customobjects/#{api_name}/import/#{id}/failures.json")
+    end
+
+    def import_custom_objects_warnings(id, api_name)
+      get("/bulk/v1/customobjects/#{api_name}/import/#{id}/warnings.json")
+    end
+  end
+end
