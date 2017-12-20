ee05e81d4e6a57c261a5dbd5f4e55a966d50911c
KARASZI István <github@spam.raszi.hu>
2017-12-20 20:43:57 +0100

    Add adapter option

--- lib/mrkt/concerns/connection.rb
+++ lib/mrkt/concerns/connection.rb
@@ -12,13 +12,12 @@ module Mrkt
         conn.request :url_encoded
 
         conn.response :logger, @logger, (@log_options || {}) if @debug
-
         conn.response :mkto, content_type: /\bjson$/
 
         conn.options.timeout = @options[:read_timeout] if @options.key?(:read_timeout)
         conn.options.open_timeout = @options[:open_timeout] if @options.key?(:open_timeout)
 
-        conn.adapter Faraday.default_adapter
+        conn.adapter @options.fetch(:adapter, Faraday.default_adapter)
       end
     end
   end
