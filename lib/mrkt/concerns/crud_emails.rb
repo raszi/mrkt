e1a97e5d76c9249af9280990e3d5de7bafe27fd8
prachi-mantri <prachi.mantri@gmail.com>
2018-02-14 04:19:42 +0530

    Test: Update email subject

--- lib/mrkt/concerns/crud_emails.rb
+++ lib/mrkt/concerns/crud_emails.rb
@@ -19,10 +19,25 @@ module Mrkt
       post("/rest/asset/v1/email/#{id}/approveDraft.json")
     end
 
+    def update_email(id)
+      post("/rest/asset/v1/email/#{id}/content.json") do
+        { subject: {type: "string", value: "Announcement Alert"}}
+      end
+    end
+
     def update_email_content(id, html_id, content, text_value = nil)
       post("/rest/asset/v1/email/#{id}/content/#{html_id}.json") do |req|
         req.headers[:content_type] = 'multipart/form-data;charset=UTF-8'
         req.body = {type: "Text", value: content}
+      end
+    end
+
+    def update_email_full_content(id, content)
+      payload = {}
+      payload[:content] = Faraday::UploadIO.new(content.path, 'text/html')
+      # payload = {content: content}
+      post("/rest/asset/v1/email/#{id}/fullContent.json", payload) do |req|
+        req.headers[:content_type] = 'multipart/form-data;charset=UTF-8'
       end
     end
 
--- lib/mrkt/errors.rb
+++ lib/mrkt/errors.rb
@@ -47,6 +47,7 @@ class Mrkt::Errors
   end
 
   def self.find_by_response_code(response_code)
+    puts response_code.inspect
     const_get(RESPONSE_CODE_TO_ERROR.fetch(response_code, 'Error'))
   end
 end
