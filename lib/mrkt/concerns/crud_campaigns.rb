3ea86311c2038dc7cc8073ab6656304a4347a617
prachi-mantri <prachi.mantri@gmail.com>
2018-02-13 06:15:58 +0530

    Update email content, schedule campaign
    IMPORTANT: while sending data to update email, make sure the content
    type is multipart/form-data

--- lib/mrkt/concerns/crud_campaigns.rb
+++ lib/mrkt/concerns/crud_campaigns.rb
@@ -10,5 +10,18 @@ module Mrkt
         }
       end
     end
+
+    def schedule_campaign(id, run_at = nil)
+      if run_at.blank?
+        run_at = 2.minutes.from_now.in_time_zone(-8).iso8601
+      end
+      post_json("/rest/v1/campaigns/#{id}/schedule.json") do
+        {
+          input: {
+            runAt: run_at
+          }
+        }
+      end
+    end
   end
 end
--- lib/mrkt/concerns/crud_emails.rb
+++ lib/mrkt/concerns/crud_emails.rb
@@ -8,13 +8,22 @@ module Mrkt
       params = {
         name: name
       }
-
       get("/rest/asset/v1/email/byName.json", params)
     end
 
     def get_email_content(id)
-
       get("/rest/asset/v1/email/#{id}/content.json")
+    end
+
+    def approve_email_draft(id)
+      post("/rest/asset/v1/email/#{id}/approveDraft.json")
+    end
+
+    def update_email_content(id, html_id, content, text_value = nil)
+      post("/rest/asset/v1/email/#{id}/content/#{html_id}.json") do |req|
+        req.headers[:content_type] = 'multipart/form-data;charset=UTF-8'
+        req.body = {type: "Text", value: content}
+      end
     end
 
   end
