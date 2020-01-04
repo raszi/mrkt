2cd44d0df3ff16a43978f786515d1ae09ea72411
KARASZI István <github@spam.raszi.hu>
2020-01-04 11:12:58 +0100

    Avoid proc

--- lib/mrkt/faraday_middleware.rb
+++ lib/mrkt/faraday_middleware.rb
@@ -2,6 +2,6 @@ require 'mrkt/faraday_middleware/response'
 
 module Mrkt
   if ::Faraday::Middleware.respond_to?(:register_middleware)
-    ::Faraday::Response.register_middleware(mkto: -> { Mrkt::FaradayMiddleware::Response })
+    ::Faraday::Response.register_middleware(mkto: Mrkt::FaradayMiddleware::Response)
   end
 end
