9d9fd39410794e0bba2c4100ccc9d0d87d7c6d78
KARASZI István <github@spam.raszi.hu>
2020-01-04 11:09:07 +0100

    Add tests for the ParamsEncoder

---
+++ spec/faraday/params_encoder_spec.rb
@@ -0,0 +1,24 @@
+describe Mrkt::Faraday::ParamsEncoder do
+  describe '.encode' do
+    let(:params) do
+      {
+        string: 'foobar',
+        number: 1,
+        boolean: true,
+        array: [1, 2, 3]
+      }
+    end
+
+    subject { described_class.encode(params) }
+
+    it { is_expected.to eq(Faraday::Utils::ParamsHash.new.merge(params.merge(array: '1,2,3')).to_query) }
+  end
+
+  describe '.decode' do
+    let(:value) { 'foo=foo&bar=bar' }
+
+    subject { described_class.decode(value) }
+
+    it { is_expected.to eq({ 'foo' => 'foo' , 'bar' => 'bar' }) }
+  end
+end
