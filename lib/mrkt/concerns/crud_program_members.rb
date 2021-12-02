58d461a024149387f46dac6776b9598454e2732b
Domenico Angilletta <domenico.angilletta@livestorm.co>
2021-12-02 17:46:03 +0100

    Add program member get method

--- lib/mrkt/concerns/crud_program_members.rb
+++ lib/mrkt/concerns/crud_program_members.rb
@@ -12,5 +12,20 @@ module Mrkt
         }
       end
     end
+
+    def get_program_members(program_id, filter_type, filter_values, fields: nil, batch_size: nil, next_page_token: nil)
+      params = {
+        filterType: filter_type,
+        filterValues: filter_values
+      }
+
+      optional = {
+        fields: fields,
+        batchSize: batch_size,
+        nextPageToken: next_page_token
+      }
+
+      get("/rest/v1/programs/#{program_id}/members.json", params, optional)
+    end
   end
 end
--- spec/concerns/crud_program_members_spec.rb
+++ spec/concerns/crud_program_members_spec.rb
@@ -95,4 +95,44 @@ describe Mrkt::CrudProgramMembers do
 
     it { is_expected.to eq(response_stub) }
   end
+
+  describe '#get_program_members' do
+    let(:filter_type) { 'leadId' }
+    let(:filter_values) { [1, 2] }
+    let(:program_id) { 1014 }
+    let(:response_stub) do
+      {
+        requestId: '4b6d#17d7c0530de',
+        result: [
+          {
+              seq: 0,
+              leadId: 1,
+              reachedSuccess: true,
+              programId: 1014,
+              acquiredBy: false,
+              membershipDate: '2021-12-02T16:22:12Z'
+          },
+          {
+              seq: 1,
+              leadId: 2,
+              reachedSuccess: true,
+              programId: 1014,
+              acquiredBy: false,
+              membershipDate: '2021-12-02T16:22:12Z'
+          }
+        ],
+        success: true,
+        moreResult: false
+    }
+    end
+    subject { client.get_program_members(program_id, filter_type, filter_values) }
+
+    before do
+      stub_request(:get, "https://#{host}/rest/v1/programs/#{program_id}/members.json")
+        .with(query: { filterType: filter_type, filterValues: filter_values.join(',') })
+        .to_return(json_stub(response_stub))
+    end
+
+    it { is_expected.to eq(response_stub) }
+  end
 end
